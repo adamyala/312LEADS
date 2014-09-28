edafcf9e58bb6d60b9ef20d91d291d6a8e698818
Adam Yala <adamkyala@gmail.com>
2014-09-28 12:31:41 -0500

    last commit before presentation

--- __init__.py
+++ __init__.py
@@ -4,18 +4,29 @@ import inspect, os
 
 from flask import Flask, render_template, request, g
 from flask.ext.mail import Mail, Message
+from pygeocoder import Geocoder
+
 app = Flask(__name__)
 app.config.update(dict(
     DEBUG = True,
-    MAIL_SERVER = 'smtp.gmail.com',
+    MAIL_SERVER = 'smtp.sendgrid.net',
     MAIL_PORT = 587,
     MAIL_USE_TLS = True,
     MAIL_USE_SSL = False,
-    MAIL_USERNAME = 'adam@adamyala.com',
-    MAIL_PASSWORD = 'XXX',
+    MAIL_USERNAME = 'azure_c49eb65c111aa3e4526967bdbd38af9d@azure.com',
+    MAIL_PASSWORD = 'lT12x6P86P3GzsN',
 ))
 
 mail = Mail(app)
+
+import csv
+def csv_to_list(file_path):
+	datafile = open(file_path, 'r')
+	datareader = csv.reader(datafile)
+	data = []
+	for row in datareader:
+		data.append(row)
+	return data
 
 app.database = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) + '/abcproject.db'
 def connect_to_database():
@@ -31,18 +42,18 @@ import ast
 def email_dump(leads):
 
 	jobdict = {
-		'Food Wholesale': '1',
-		'Restaurant Supplier': ['1', '2', '4', '6'],
-		'HVAC/Refregiration': ['2', '3'],
+		'Food Wholesaler': '1',
+		'Restaurant Supply Company': ['1', '2', '4', '6'],
+		'HVAC Company/Refrigeration Supply': ['2', '3'],
 		'Pharmacy': '5',
-		'Construction': ['11', '26', '34', '35', '36', '37', '38'],
+		'Construction Company': ['11', '26', '34', '35', '36', '37', '38'],
 		'Electrician': ['2', '17', '22', '36'],
 		'Staffing Agency': ['21', '23', '27', '31', '33', '41', '42', '44', '70'],
-		'Appliance Repair': ['2', '3'],
-		'Property Management': ['19'],
-		'Waste Management': ['19','20'],
-		'Pest Control': ['18','23'],
-		'Plumbing': ['7', '9', '10', '11', '12', '22', '24', '26', '27', '38']
+		'Appliance Repair Company': ['2', '3'],
+		'Property Management Company': ['19'],
+		'Waste Management Company': ['19','20'],
+		'Pest Control Company': ['18','23'],
+		'Plumbing Company': ['7', '9', '10', '11', '12', '22', '24', '26', '27', '38']
 	}
 
 	if request.method == 'POST':
@@ -51,86 +62,131 @@ def email_dump(leads):
 			subject[field] = request.form[field]
 	g.db = connect_to_database()
 
-	i = (subject['zipcode'],)
-	tempbusinesses = g.db.execute("SELECT * FROM business WHERE zipcode=?", i)
+	main_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 
-	violations = []
+	# licenses = csv_to_list(main_dir + '/Licenses.csv')
+
+	# violations = csv_to_list(main_dir + '/Food_Inspections.csv')
+
+	# result = []
+
+	# codelist = jobdict[subject['occupation']]
+
+	# bizlist = []
+	# i = 0
+	# while len(bizlist) < 10:
+	# 	if licenses[i][1] == subject['zipcode']:
+	# 		bizlist = bizlist.append(licenses[i])
+
+	# loop through unique license list
+		# only take businesses that match zip
+		# stop at 10
+
+	result = []
+	# for biz in bizlist:
+	# 	print 'biz is' + biz
+	# 	for violation in violations:
+	# 		print violation + 'for ' + biz
+	# 		if biz[0] == violation[1] and biz[1] == violation[5]:
+	# 			comments = violation[6].rsplit('|')
+	# 			bizcomments = []
+	# 			for comment in comments:
+	# 				if comment.strip()[0:2].rstrip('.') in jobdict:
+	# 					print 'writing comment'
+	# 					bizcomments = bizcomments.append(comment[comment.find('Comments:')+9:].strip())
+	# 	result = result.append([biz[3]+" - "+biz[2],bizcomments])
+
+	# print result
+
+	# load ordered list
+		# loop through rows, take comments that match jobdict
+
+
+	# for row in violations:
+
+	# 	if violation[9] == "":
+	# 		zipcode = Geocoder.geocode(violation[6] + 'CHICAGO IL')[0].postal_code
+	# 	else:
+	# 		zipcode = violation[9]
+
+	# 	business = [
+	# 		violation[0],
+	# 		violation[3],
+	# 		violation[6] + 'CHICAGO IL',
+	# 		zipcode, violation[4]
+	# 	]
+
+	# 	comments = violation[13].rsplit('|')
+	# 	# [
+	# 	# 	violation[3],
+	# 	# 	violation[10][0:violations[0][10].find('T')],
+	# 	# 	line[line.find('Comments:')+9:].strip(),
+	# 	# 	line.strip()[0:2].rstrip('.'))
+	# 	# ]
+
+	# 	if violations[9] == subject['zipcode'] and violations[3] in codelist:
+	# 		result = result.apprend(violations[1] + ' - ' + violations[6] + ' CHICAGO IL')
+
+
+
+	i = (subject['zipcode'].encode('ascii','ignore'),)
+	print i
+	tempbusinesses = g.db.execute("SELECT * FROM business WHERE zipcode='60609'")#?", i)
+	businesses = tempbusinesses.fetchall()
+	print businesses
+
+	# violations = []
 	j = jobdict[subject['occupation']]
 	for code in j:
-		tempviolations = g.db.execute("SELECT * FROM violations WHERE violationnum=?", [code]).fetchall()
-
-		violationspart = [list(row) for row in tempviolations]
-
-		# for row in violationspart:
-			# print row
-			# violations = violations.append(row)
-
-
-
-
-
-
-		# tempviolations = tempviolations.fetchall()
-		# print violations
-		# for row in tempviolations:
-		# 	print row
-		# 	cheese = [""] * len(list(row))
-		# 	for mold in range(len(list(row))):
-		# 		print mold
-		# 		cheese[mold] = "" + list(row)[mold]
-		# 	violations = violations.append(cheese)
-		# 	print cheese
-		# 	print violations
-		# 	# print list(row)
-		# # violations = violations.append([list(row) for row in tempviolations])
-
-		# violations = violations.append(ast.literal_eval(tempvio))
-	# businesses = g.db.execute("SELECT * FROM violations WHERE violationnum IN (?)", (j,))
-	# businesses = g.db.execute("SELECT * FROM violations WHERE violationnum IN ()")
-
-	businesses = tempbusinesses.fetchall()
-	# violations = tempviolations.fetchall()
+		violations = g.db.execute("SELECT * FROM violations WHERE violationnum=?", [code]).fetchall()
+		for row in violations:
+			row = list(row)
+			for vio in row:
+				vio = vio.encode('ascii','ignore')
 
 	result = []
 	businesses = [list(row) for row in businesses]
-	# violations = [list(row) for row in violations]
 	for biz in businesses:
 		result.append(biz[0]+" - "+biz[2]+biz[3])
 		for vio in violations:
-			temp = "" + vio[0]
+			temp = vio[0]
 			if biz[1] == temp:
 				result.append("  - "+vio[2])
 
-	print 'started function'
+	emailmess = """
+			<table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
+				<tr>
+					<td bgcolor="#DF6060" style="padding: 15px 15px 15px 15px;">
+					</td>
+				</tr>
+				<tr>
+					<td bgcolor="#333" style="text-align:center; color:white">
+						<h1><strong>312</strong>LEADS</h1>
+					</td>
+				</tr>
+				<tr>
+					<td bgcolor="#333" style="color:white; text-align:left; padding-left: 5%; padding-right: 5%; padding-bottom: 2%">"""
+
+	for row in result:
+		emailmess = emailmess + """<p style="color:white">""" + row + "</p>"
+
+	emailmess = emailmess + """					</td>
+				</tr>
+				<tr>
+					<td bgcolor="#333" style="text-align:center; color:#999; padding: 15px 15px 15px 15px;">
+						Created at the MonkeyBars Open Build Hackathon
+					</td>
+				</tr>
+			</table>
+		"""
+
 	msg = Message("Your Leads", sender="leads@312LEADS.com",
-		html="""<table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
-	<tr>
-		<td bgcolor="#DF6060" style="padding: 15px 15px 15px 15px;">
-		</td>
-	</tr>
-	<tr>
-		<td bgcolor="#333" style="text-align:center; color:white">
-			<h1><strong>312</strong>LEADS</h1>
-		</td>
-	</tr>
-	<tr>
-		<td bgcolor="#333" style="color:white; text-align:left">
-
-		</td>
-	</tr>
-	<tr>
-		<td bgcolor="#333" style="text-align:center; color:#999; padding: 15px 15px 15px 15px;">
-			Created at the MonkeyBars Open Build Hackathon
-		</td>
-	</tr>
-</table>""",
+		html=emailmess,
 		recipients=[subject['email']])
-	print 'made email'
 	msg.body = ''.join(result)
-	# mail.send(msg)
-	print 'sent email'
+	mail.send(msg)
 
-	return render_template('email_dump.html', leads=result)
+	return render_template('email_dump.html')
 
 if __name__ == "__main__":
 	app.debug = True
--- import.py
+++ import.py
@@ -4,13 +4,28 @@ import requests
 import json
 from pygeocoder import Geocoder
 
+import csv
+def csv_to_list(file_path):
+	datafile = open(file_path, 'r')
+	datareader = csv.reader(datafile)
+	data = []
+	for row in datareader:
+		data.append(row)
+	return data
+
 main_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 
+print main_dir
 r = requests.get('http://data.cityofchicago.org/resource/4ijn-s7e5.json')
 
 violations = r.json()
 
+# violations = csv_to_list(main_dir + '/Food_Inspections.csv')
+# print violations
+
 # print violations[0]
+
+# with open("Output.txt", "w") as text_file:
 
 with sqlite3.connect(main_dir + '/abcproject.db') as connection:
 	c = connection.cursor()
@@ -26,21 +41,38 @@ with sqlite3.connect(main_dir + '/abcproject.db') as connection:
 		except:
 			zipcode = Geocoder.geocode(violation['address'] + 'CHICAGO IL')[0].postal_code
 
+		# if violation[9] == "":
+		# 	zipcode = Geocoder.geocode(violation[6] + 'CHICAGO IL')[0].postal_code
+		# else:
+		# 	zipcode = violation[9]
+
 		try:
 			facility = violation['facility_type']
 		except:
 			facility = ""
 
-		print zipcode
+		# facility = violation[4]
+
+		# text_file.write(violation['dba_name'] + " " + zipcode)
+		# if zipcode == None: zipcode = ""
+		# print violation[0]
+		# print zipcode
+		# text_file.write(violation[0] + ' ' + zipcode)
+
+		# text_file.write('\n')
+
+		# print violation['dba_name'] + " " + zipcode
 
 		c.execute("""INSERT INTO business VALUES(?,?,?,?,?)""", (violation['dba_name'],violation['license_'],violation['address'] + 'CHICAGO IL', zipcode, facility))
+		# c.execute("""INSERT INTO business VALUES(?,?,?,?,?)""", (violation[0],violation[3],violation[6] + 'CHICAGO IL', zipcode, facility))
 
 		try:
 			comments = violation['violations'].rsplit('|')
+			# comments = violation[13].rsplit('|')
 
 			for line in comments:
 				c.execute("""INSERT INTO violations VALUES(?,?,?,?)""", (violation['license_'], violation['inspection_date'][0:violations[0]['inspection_date'].find('T')], line[line.find('Comments:')+9:].strip(), line.strip()[0:2].rstrip('.')))
-				
+				# c.execute("""INSERT INTO violations VALUES(?,?,?,?)""", (violation[3], violation[10][0:violations[0][10].find('T')], line[line.find('Comments:')+9:].strip(), line.strip()[0:2].rstrip('.')))
 		except:
 			pass
 
